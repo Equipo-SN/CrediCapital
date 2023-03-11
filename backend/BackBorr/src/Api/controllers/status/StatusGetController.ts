626cda5dfd6c1049f6c263806e51e5b10876d42e
Jose <docs.huehue@gmail.com>
2023-03-11 15:06:17 -0600

    Fis status controller

--- backend/BackBorr/src/Api/controllers/status/StatusGetController.ts
+++ backend/BackBorr/src/Api/controllers/status/StatusGetController.ts
@@ -1,10 +1,8 @@
-import { Request, Response } from "express";
-import { Controller } from "../Controller";
+import { Request, Response } from 'express';
+import { Controller } from '../Controller';
 
-
-export default class StatusGetController implements Controller{
-    async run(req: Request, res: Response): Promise<void> {
-        res.status(200).send();
-    }
-
+export default class StatusGetController implements Controller {
+	async run(req: Request, res: Response): Promise<void> {
+		res.status(200).send({ "status": "online" });
+	}
 }
